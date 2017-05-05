12465d7f6ebe69f9159cebbe69fa9d9eba60bf9b
Swift Funcional <info@swiftfuncional.com>
2017-05-05 23:47:20 +0200

    Create || (OR) validator

--- Monoides.playground/Sources/Validator.swift
+++ Monoides.playground/Sources/Validator.swift
@@ -16,6 +16,22 @@ public func &&<T, E>(
 	}
 }
 
+public func ||<T, E>(
+	_ firstValidator: @escaping Validator<T, E>,
+	_ secondValidator: @escaping Validator<T, E>) -> Validator<T, E> {
+
+	return { t in
+		let result = firstValidator(t)
+
+		switch result {
+		case .Failure:
+			return secondValidator(t)
+		case .Success:
+			return result
+		}
+	}
+}
+
 public func validate<T, E>(_ reason: E, _ condition: @escaping (T) -> Bool) -> Validator<T, E> {
 	return { condition($0) ? .Success($0) : .Failure(reason) }
 }
