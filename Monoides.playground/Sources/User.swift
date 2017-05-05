1a80b2ca53f061ca553fe15d937dbc12ecb42353
Swift Funcional <info@swiftfuncional.com>
2017-05-05 23:43:43 +0200

    Add two attribute to User: premium and newsletter

--- Monoides.playground/Sources/User.swift
+++ Monoides.playground/Sources/User.swift
@@ -1,10 +1,14 @@
 public struct User {
 	public let name: String
 	public let password: String
+	public let premium: Bool
+	public let newsletter: Bool
 
-	public init(name: String, password: String) {
+	public init(name: String, password: String, premium: Bool, newsletter: Bool) {
 		self.name = name
 		self.password = password
+		self.premium = premium
+		self.newsletter = newsletter
 	}
 }
 
